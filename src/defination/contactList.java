5c0cf47633b6ad2624bc83f63193f70c3d4aa13c
Abhishek15725 <Abhishek15725@gmail.com>
2020-02-21 22:21:38 +0530

    add contact

--- src/defination/contactList.java
+++ src/defination/contactList.java
@@ -13,4 +13,22 @@ public class ContactList<Person> implements ContactListADT<Person> {
             head = new Node<>(personData, head);
             size++;
         }
+    private void addAfter(Node<Person> personNode, Person personData) {
+        personNode.next = new Node<>(personData, personNode.next);
+        size++;
+    }
+
+    public int getSize() {
+        return size;
+    }
+    public void add(int index, Person personData) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException(Integer.toString(index));
+        } else if (size == 0) {
+            addFirst(personData);
+        } else {
+            Node<Person> temp = getNode(index - 1);
+            addAfter(temp, personData);
+        }
+    }
 }
